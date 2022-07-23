f1208953bd6f77865065a17ddb0d70e3ff0167d2
[GPL]Ed <sanduedi309@gmail.com>
2022-07-23 12:44:18 +0000

    remove a.out

--- keys.h
+++ keys.h
@@ -131,7 +131,7 @@ input(void)
 			case 's':
 				clrscr();
 				printf("Which signal to send to process %u?\n\
-=================================================================\n\
+====================================================================\n\
  1 - SIGHUP     2 - SIGINT   3 - SIGQUIT   4 - SIGILL    5 - SIGTRAP\n\
  6 - SIGABRT    7 - SIGBUS   8 - SIGFPE    9 - SIGKILL  10 - SIGUSR1\n\
 11 - SIGSEGV   12 - SIGUSR2 13 - SIGPIPE  14 - SIGALRM  15 - SIGTERM\n\
